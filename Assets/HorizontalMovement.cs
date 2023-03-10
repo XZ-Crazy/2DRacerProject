ba95ee9f715543f24c6119875b1a59cb72ff82e2
Aurum <47676248+XZ-Crazy@users.noreply.github.com>
2023-03-09 16:54:06 -0800

    Added Objects to Aurum Debug Scene

---
+++ Assets/HorizontalMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+    float Velocity = 5;
+    int Gear = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
